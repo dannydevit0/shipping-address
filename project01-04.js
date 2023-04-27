66e3420ffb39ea9ee9aff66ac9c16033de4db112
dannydevit0 <122465014+dannydevit0@users.noreply.github.com>
2023-04-27 10:39:43 -0400

    Add files via upload
    shipping-address 04/27/2024

---
+++ project01-04.js
@@ -0,0 +1,17 @@
+/* 
+ Chapter 1
+    Hands-On Project 1-4
+
+    Author: Olivia Gavin
+    Date: 04/25/2023  
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+
+let homeStreet = "1 Main St.", homeCity = "Siclia", homeState = "MA", homeCode = "02130";
+
+let workStreet = "15 Oak Ln.", workCity = "Central City", workState = "MA", workCode = "02104";
+
+
